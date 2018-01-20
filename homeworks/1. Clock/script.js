7cd0f469b44c2b76fed743eff5cbbffa0f82bdfa
Konstantin <DoshikKing@github.com>
2018-01-20 19:11:38 +0300

    Function for Stripes in clock
    Works

--- homeworks/1. Clock/script.js
+++ homeworks/1. Clock/script.js
@@ -2,11 +2,12 @@ var canvas = document.getElementById("sandbox");
 var context = canvas.getContext("2d");
 
 var circle = new Path2D();
-circle.arc(150, 150, 100, 0, 2 * Math.PI);
+var R1 = 150;
+circle.arc(R1, R1, R1, 0, 2 * Math.PI);
 context.stroke(circle);
 
 function Stripes() {
-    var R = 300 / 2, d, angle, pX, pY, qX, qY
+    var R = R1, d, angle, pX, pY, qX, qY
     var d, angle, pX, pY, qX, qY
     for(d = 0; d < 60; ++d) {
         angle = (d / 60) * (2 * Math.PI);
@@ -16,6 +17,19 @@ function Stripes() {
         qY = 0.9 * pY;
         pX += R; pY += R;
         qX += R; qY += R;  
+
+        var canvas = document.getElementById("sandbox");
+        var context = canvas.getContext("2d");
+
+        var line = new Path2D();
+
+        line.moveTo(pX, pY);
+        line.lineTo(qX, qY);
+
+        context.lineWidth = ((d % 5) == 0) ? 3 : 1; 
+        context.stroke(line);
     }
 }
+
+Stripes();
 
