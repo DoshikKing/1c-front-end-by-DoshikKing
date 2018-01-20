03d1fab8e6b3a0cfd3554162ef77aa715b49c630
Konstantin <DoshikKing@github.com>
2018-01-20 18:41:43 +0300

    Revert "- context.stroke(circle);"
    This reverts commit aee6c68e910f63389fc715e53bc2aa70b9998e21.

--- homeworks/1. Clock/script.js
+++ homeworks/1. Clock/script.js
@@ -3,7 +3,7 @@ var context = canvas.getContext("2d");
 
 var circle = new Path2D();
 circle.arc(150, 150, 100, 0, 2 * Math.PI);
-
+context.stroke(circle);
 
 function Stripes() {
     var R = 300 / 2, d, angle, pX, pY, qX, qY
