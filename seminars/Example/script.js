10b47e93b7866f102c293958a82004e852e13496
Konstantin <DoshikKing@github.com>
2018-01-28 13:45:36 +0300

    Adding comments

--- seminars/Example/script.js
+++ seminars/Example/script.js
@@ -1,25 +1,32 @@
 var array = [1, 2, 3, 4, 5];
 
+// Получаем числовой остаток, ввыодя числа с числовым остатком 1 из массива
+
 array.filter(function(x) {
     return x % 2 == 1;
 });
+
+// Домножаем весь массив на 2
 
 array.map(function(x) {
     return x * 2
 });
 
+// Выводим каждое значение массива
+
 array.forEach(function(x) {
     console.log(x);
 });
 
-//Сортирует массив по порядку
+// Сортирует массив по порядку
 array.sort();
 
-//Переворачивает массив
+// Переворачивает массив
 
 array.reverse();
 
-//return temperature > 0 ? "Water" : "Ice"; - тоже самое что if{}else{}
+// return temperature > 0 ? "Water" : "Ice"; - тоже самое что if{}else{}
+// return temperature > - тернарный оператор
 
 function getWaterState(temperature) {
     return temperature > 0 ? "Water" : "Ice";
